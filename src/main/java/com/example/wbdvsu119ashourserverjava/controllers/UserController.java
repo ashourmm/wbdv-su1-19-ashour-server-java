5f31b497370a41d78ff6f07c278c4c2760561aee
ashourm <ashour.m@husky.neu.edu>
2019-06-11 18:53:15 -0400

    course repository added

--- src/main/java/com/example/wbdvsu119ashourserverjava/controllers/UserController.java
+++ src/main/java/com/example/wbdvsu119ashourserverjava/controllers/UserController.java
@@ -12,59 +12,71 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.wbdvsu119ashourserverjava.repositories.UserRepository;
+import jdk.nashorn.internal.runtime.options.Option;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import com.example.wbdvsu119ashourserverjava.models.User;
 
 @RestController
 public class UserController {
-	private User[] users = {
-            new User(1, "alice", "alice", "Alice", "Wonderland", "Student"),
-            new User(2, "bob", "bob", "Bob", "The builder", "Student"),
-            new User(3, "charlier", "charlier", "Charlier", "Brown", "Student")
+    private User[] users = {
+            new User(1, "alice", "alice", "Alice", "Wonderland"),
+            new User(2, "bob", "bob", "Bob", "The builder"),
+            new User(3, "charlier", "charlier", "Charlier", "Brown")
     };
     List<User> userArrayList = new ArrayList<User>(Arrays.asList(users));
-	
-	@GetMapping("/users")
-	public List<User> findAllUser() {
-		return userArrayList;
-	}
-	
-	@DeleteMapping("/users/{userId}")
-    public List<User> deleteUser(@PathVariable("userId") long userId) {
-        User u = null;
-        for(User user:userArrayList) {
-            if(user.getId() == userId) {
-                u = user;
-            }
-        }
-        userArrayList.remove(u);
-        return userArrayList;
-    }
-	
-	@PostMapping("/users")
-	public List<User> createUser(@RequestBody User user) {
-		userArrayList.add(user);
-		return userArrayList;
-	}
 
-	 @PutMapping("/users/{userId}") 
-	 public List<User> updateUser(@PathVariable("userId") long userId, @RequestBody User user) {
-		String firstName = user.getFirstName();
-		String lastName = user.getLastName();
-		String username = user.getUsername();
-		String role = user.getRole();
-		
-		for(User u:userArrayList) {
-			if(u.getId() == userId) {
-				u.setFirstName(firstName);
-				u.setLastName(lastName);
-				u.setUsername(username);
-				u.setRole(role);
-			}
-		}
-		
-		return userArrayList;
-		 
-	 }
-	
- 
+    @Autowired
+    UserRepository repository;
+
+    @GetMapping("/users")
+    public List<User> findAllUsers() {
+        return (List<User>)repository.findAll();
+//        return userArrayList;
+    }
+
+    @GetMapping("/users/username/{username}")
+    public User findUserByUsername(@PathVariable("username") String username) {
+        return repository.findUserByUsername(username);
+    }
+
+    @GetMapping("/users/username/{username}/password/{password}")
+    public User findUserByCredentials(
+            @PathVariable("username") String username,
+            @PathVariable("password") String password) {
+        return repository.findUserByCredentials(username, password);
+    }
+
+//     @GetMapping("/users/{userId}")
+//     public User findUserById(@PathVariable("userId") Long userId) {
+//         Optional<User> optional = repository.findById(userId);
+//         return optional.get();
+//     }
+
+//     @DeleteMapping("/users/{userId}")
+//     public List<User> deleteUser(
+//             @PathVariable("userId") Long userId) {
+//         repository.deleteById(userId);
+//         return findAllUsers();
+//     }
+
+//     @PutMapping("/users/{userId}")
+//     public List<User> updateUser(
+//             @PathVariable("userId") Long userId,
+//             @RequestBody User userUpdates) {
+//         Optional<User> optional = repository.findById(userId);
+//         User user = optional.get();
+//         user.setPassword(userUpdates.getPassword());
+//         repository.save(user);
+//         return findAllUsers();
+//     }
+    
+//     @PostMapping("/users")
+//     public List<User> createUser(@RequestBody User user) {
+//         repository.save(user);
+// //        userArrayList.add(user);
+//         return findAllUsers();
+//     }
+    
 }
